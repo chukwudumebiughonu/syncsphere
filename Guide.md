749875f33de60c2c10f2d0334f58b0326ed21352
chukwudumebiughonu <plasdexcel14@gmail.com>
2024-04-03 13:34:31 +0100

    initialize project

---
+++ Guide.md
@@ -0,0 +1,6 @@
+Create application wiht : bunx --bun create-next-app@latest . --typescript --tailwind --eslint
+Initialize Shadcn-ui with: bunx --bun shadcn-ui@latest init
+
+Initialize Aceternity Ui with: bun add framer-motion clsx tailwind-merge
+
+Initialize theme with : bun add next-themes 
---
+++ src/app/layout.tsx
@@ -0,0 +1,32 @@
+import type { Metadata } from "next";
+import { ThemeProvider } from "@/providers/theme-provider"
+import { DM_Sans } from "next/font/google";
+import "./globals.css";
+
+const font = DM_Sans({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "SyncSphere",
+  description: "Automate Your Work with SyncSphere",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={font.className}>
+      <ThemeProvider
+            attribute="class"
+            defaultTheme="dark"
+            enableSystem
+            disableTransitionOnChange
+          >
+        {children}
+      </ThemeProvider>
+        </body>
+    </html>
+  );
+}
